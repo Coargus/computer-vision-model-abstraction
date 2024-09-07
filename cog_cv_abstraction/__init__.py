8623b309f51b802b177746d09bc6c40012d7c8b0
Choi Minkyu (mc76728) <mc76728@austin.utexas.edu@ece-a52150.austin.utexas.edu>
2024-09-07 20:35:31 +0000

    add frame schema

--- cog_cv_abstraction/__init__.py
+++ cog_cv_abstraction/__init__.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from .enum import status
 from .image import detection
-from .schema import dataset, detected_object
+from .schema import dataset, detected_object, frame
 
-__all__ = ["detection", "status", "detected_object", "dataset"]
-__version__ = "0.0.1"
+__all__ = ["detection", "status", "detected_object", "dataset", "frame"]
+__version__ = "0.0.3-dev"
