34fbcca887674a9d58f5d108a9d702974ba67637
Minkyu Choi <minkyu.choi07@gmail.com>
2024-09-08 03:17:51 +0000

    fix import error

--- cog_cv_abstraction/__init__.py
+++ cog_cv_abstraction/__init__.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from .enum import status
 from .image import detection
-from .schema import dataset, detected_object, frame
+from .schema import dataset, detected_object, video_frame
 
-__all__ = ["detection", "status", "detected_object", "dataset", "frame"]
+__all__ = ["detection", "status", "detected_object", "dataset", "video_frame"]
 __version__ = "v0.0.4-dev"
