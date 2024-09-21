867cd2b37c70a551d9db8f14f61c9cbfff1131bf
Minkyu Choi <minkyu.choi07@gmail.com>
2024-09-21 03:38:45 +0000

    add VLM base class (v0.0.5-dev)

--- cog_cv_abstraction/__init__.py
+++ cog_cv_abstraction/__init__.py
@@ -7,4 +7,4 @@ from .image import detection
 from .schema import dataset, detected_object, video_frame
 
 __all__ = ["detection", "status", "detected_object", "dataset", "video_frame"]
-__version__ = "v0.0.4-dev"
+__version__ = "v0.0.5-dev"
